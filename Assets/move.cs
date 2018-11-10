0c0e6df8f32472435758bce61fbcb549bddf4de6
Stefanos Ioannou <bluemindset@outlook.com>
2018-11-10 22:55:37 +0200

    a

---
+++ Assets/move.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public float force = 1000f;
+    public Rigidbody r;
+    private void OnMouseDown()
+    {
+        this.GetComponent<Rigidbody>().AddForce(0,0,-force, ForceMode.Acceleration);
+        
+    }
+}
